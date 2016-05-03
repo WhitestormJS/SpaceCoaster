c79a6e73b49737c640f630100c31b6dc8258c8c5
Estuardo Diaz <esjoando555@gmail.com>
2016-05-03 08:29:19 -0600

    Adds lights to the scene

--- src/scripts/lights.js
+++ src/scripts/lights.js
@@ -1,12 +1,12 @@
 const ambient = game.AmbientLight({
   light: {
-    color: 0xe4d04B,
-    intensity: 0.1,
+    color: 0x83DCF9,
+    intensity: 0.2,
 
     pos: {
-      x: 0,
-      y: 0,
-      z: 150
+        x: 0,
+        y: 120,
+        z: 0,
     },
 
     target: {
@@ -16,3 +16,30 @@ const ambient = game.AmbientLight({
     }
   }
 });
+
+const spot = game.SpotLight( {
+    light: {
+        color: 0x83DCF9,
+        intensity: 0.3,
+        distance: 500,
+    },
+
+    shadowmap: {
+        width: 2048,
+        height: 2048,
+        top: 0,
+        fov: 90
+    },
+
+    pos: {
+        x: 160, // 100,
+        y: 120, // 30,
+        z: 160, // 100
+    },
+
+    target: {
+        x: 0,
+        y: 10,
+        z: 0
+    }
+} );
