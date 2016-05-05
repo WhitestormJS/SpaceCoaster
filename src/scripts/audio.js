32a29fce7daa92a09c418c4599e47801381e6a82
Estuardo Diaz <esjoando555@gmail.com>
2016-05-04 20:24:21 -0600

    Adds flyby track

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -1,5 +1,6 @@
 const track = new THREE.AudioListener();
 const soundtrack = new THREE.Audio(track);
+const shipSound = new THREE.Audio(track);
 const camera = game.getCamera();
 const loader = new THREE.AudioLoader();
 
@@ -11,4 +12,10 @@ loader.load('assets/audio/365-days-in-space.ogg',
  function(audioBuffer){
   soundtrack.setBuffer( audioBuffer );
 	soundtrack.play();
+}, function(xhr){}, function(xhr){console.log( 'An error happened' );});
+
+loader.load('assets/audio/flyby.ogg',
+function(audioBuffer){
+  shipSound.setBuffer(audioBuffer);
+  shipSound.loaded = true;
 }, function(xhr){}, function(xhr){console.log( 'An error happened' );});
